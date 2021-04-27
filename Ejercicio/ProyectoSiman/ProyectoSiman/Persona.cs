8ccf9bb66156eb862c112d0ab8bad363f0ba6def
Brayan Raudales <Brayanmejia28@gmail.com>
2021-04-26 23:10:07 -0600

    personas siman

--- Ejercicio/ProyectoSiman/ProyectoSiman/Persona.cs
+++ Ejercicio/ProyectoSiman/ProyectoSiman/Persona.cs
@@ -3,7 +3,7 @@ public class Persona
 {
     public int Codigo { get; set; }
     public string Nombre { get; set; }
-    public string Telefono { get; set; }
+   
 
     public void EnviarNotificacion()
     {
